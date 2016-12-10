1a970bed0e5491a2a8119759a4042cc7aaca6042
Andrejs Burenins <burenins@gmail.com>
2016-12-10 20:08:28 +0200

    codility impl classes

---
+++ src/codility/CodilitySolution.java
@@ -0,0 +1,22 @@
+package codility;
+
+public class CodilitySolution {
+	
+	
+	public int solution(int[] a){
+		int result = 0;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		CodilitySolution solution = new CodilitySolution();
+		
+		int [] a = {10, 2, 5, 1, 8, 20};
+		System.err.println((solution.solution(a)));
+		
+		a = new int[]{10, 50, 5, 1};
+		System.err.println((solution.solution(a)));
+	}
+
+}
